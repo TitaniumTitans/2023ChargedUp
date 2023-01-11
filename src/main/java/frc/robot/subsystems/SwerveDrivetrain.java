dfe89e0bad2a940477c3b54c44159bb6dde53cfb
GearBoxFox <98189655+GearBoxFox@users.noreply.github.com>
2023-01-10 19:53:43 -0500

    Removed uneeded example method

--- src/main/java/frc/robot/subsystems/SwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/SwerveDrivetrain.java
@@ -126,16 +126,6 @@ public class SwerveDrivetrain extends SubsystemBase{
         });
   }
 
-  /**
-   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
-   *
-   * @return value of some boolean subsystem state, such as a digital sensor.
-   */
-  public boolean exampleCondition() {
-    // Query some boolean state, such as a digital sensor.
-    return false;
-  }
-
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
