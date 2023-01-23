9676bae10759ad111d623db9cdfd2e0e5e1c12d7
GearBoxFox <98189655+GearBoxFox@users.noreply.github.com>
2023-01-23 18:41:27 -0500

    default trajectory using pathplannerlib

--- src/main/java/frc/robot/commands/Autonomous/AutoUtils.java
+++ src/main/java/frc/robot/commands/Autonomous/AutoUtils.java
@@ -2,6 +2,13 @@ package frc.robot.commands.Autonomous;
 
 import java.util.List;
 
+import com.pathplanner.lib.PathConstraints;
+import com.pathplanner.lib.PathPlanner;
+import com.pathplanner.lib.PathPlannerTrajectory;
+import com.pathplanner.lib.PathPoint;
+import com.pathplanner.lib.commands.PPSwerveControllerCommand;
+
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
@@ -11,6 +18,8 @@ import edu.wpi.first.math.trajectory.TrajectoryGenerator;
 import edu.wpi.first.math.trajectory.TrajectoryParameterizer.TrajectoryGenerationException;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
 import frc.robot.Constants;
 import frc.robot.Constants.AutoConstants;
@@ -19,29 +28,27 @@ import frc.robot.subsystems.Swerve.SwerveDrivetrain;
 
 
 public class AutoUtils {
-    private final static TrajectoryConfig defaultConfig = new TrajectoryConfig(
-        AutoConstants.kMaxVelocityMPS, AutoConstants.kMaxAccerlerationMPS)
-        .setKinematics(DriveConstants.kDriveKinematics);
+    private final static PathConstraints defaultConfig = new PathConstraints(
+        AutoConstants.kMaxVelocityMPS, AutoConstants.kMaxAccerlerationMPS);
 
-    private final static Trajectory defaultTrajectory = TrajectoryGenerator.generateTrajectory(
-        new Pose2d(), 
-        List.of(new Translation2d(20, 0)), 
-        new Pose2d(new Translation2d(0, 0), new Rotation2d()), 
-        defaultConfig);
+    private final static PathPlannerTrajectory defaultTrajectory = PathPlanner.generatePath(
+        defaultConfig,
+        new PathPoint(new Translation2d(0, 0), Rotation2d.fromDegrees(0)), 
+        new PathPoint(new Translation2d(Units.feetToMeters(3), Units.feetToMeters(1)), Rotation2d.fromDegrees(0)));
     
     public static Command getDefaultTrajectory(SwerveDrivetrain swerve){
-        SwerveControllerCommand swerveController = new SwerveControllerCommand(
-            defaultTrajectory, 
-            swerve::getPose,
-            DriveConstants.kDriveKinematics,
-            AutoConstants.kXController,
-            AutoConstants.kYController,
-            AutoConstants.kThetaController,
+        return new SequentialCommandGroup(
+            new InstantCommand(() -> swerve.setPose(defaultTrajectory.getInitialHolonomicPose())),
+
+            new PPSwerveControllerCommand(defaultTrajectory, 
+            swerve::getPose, 
+            DriveConstants.kDriveKinematics, 
+            AutoConstants.kXController, 
+            AutoConstants.kYController, 
+            new PIDController(0, 0, 0), 
             swerve::setModuleStates,
-            swerve);
-
-            swerve.setPose(defaultTrajectory.getInitialPose());
-
-            return swerveController.andThen(() -> swerve.drive(0, 0, 0));
+            true,
+            swerve)
+        );
     }
 }
