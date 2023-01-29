75e21b01ba3f964275c4a2bbcf187f9f32b728bd
GearBoxFox <98189655+GearBoxFox@users.noreply.github.com>
2023-01-29 13:48:27 -0500

    Revert "Feat: Setup AutoFactory.java to expand for later"

--- src/main/java/frc/robot/commands/Autonomous/AutoUtils.java
+++ src/main/java/frc/robot/commands/Autonomous/AutoUtils.java
@@ -1,65 +1,67 @@
 package frc.robot.commands.Autonomous;
 
 import java.util.HashMap;
-
-import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
+import java.util.List;
 
 import com.pathplanner.lib.PathConstraints;
 import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
+import com.pathplanner.lib.PathPoint;
 import com.pathplanner.lib.commands.FollowPathWithEvents;
 import com.pathplanner.lib.commands.PPSwerveControllerCommand;
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.math.trajectory.TrajectoryParameterizer.TrajectoryGenerationException;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
+import frc.robot.Constants;
 import frc.robot.Constants.AutoConstants;
 import frc.robot.Constants.DriveConstants;
 import frc.robot.subsystems.Swerve.SwerveDrivetrain;
 
 
-public class AutoFactory {
+public class AutoUtils {
     // Default Constants
     private final static PathConstraints defaultConfig = new PathConstraints(
         AutoConstants.kMaxVelocityMPS, AutoConstants.kMaxAccelerationMPS);
         
+
+    private final static PathPlannerTrajectory defaultTrajectory = PathPlanner.generatePath(
+        defaultConfig,
+        new PathPoint(new Translation2d(0, 0), Rotation2d.fromDegrees(0)), 
+        new PathPoint(new Translation2d(Units.feetToMeters(3), Units.feetToMeters(1)), Rotation2d.fromDegrees(0)));
+    
     private static final PathPlannerTrajectory defaultAutoGen = PathPlanner.loadPath("DefaultPath", defaultConfig);
-    private static final HashMap<String, Command> defaultEventMap = new HashMap<>();
 
-    private final SwerveDrivetrain m_swerveInstance;
-
-    //Sendable chooser
-    private LoggedDashboardChooser<Command> m_chooser;
-
-    public AutoFactory(SwerveDrivetrain swerve){
-        m_swerveInstance = swerve;
-
-        defaultEventMap.put("Event 1", new PrintCommand("Marker 1"));
-        defaultEventMap.put("event 2", new PrintCommand("Marker 2"));
-
-        m_chooser = new LoggedDashboardChooser<>("Auto Routines");
-
-        configSendableChooser();
-    }
-
-    private void configSendableChooser(){
-        //Default options, maybe remove before comps
-        m_chooser.addDefaultOption("Default Option", getDefaultTrajectory(m_swerveInstance));
-        m_chooser.addOption("Default Events", getDefaultPathWithEvents(m_swerveInstance));
-    }
-
-    public Command getAuto(){
-        return m_chooser.get();
-    }
 
     //Default getters
     public static Command getDefaultTrajectory(SwerveDrivetrain swerve){
-        return getAutoRoutine(defaultAutoGen, swerve);
+        return new SequentialCommandGroup(
+            new InstantCommand(() -> swerve.resetPose(defaultAutoGen.getInitialHolonomicPose())),
+
+            new PPSwerveControllerCommand(defaultAutoGen, 
+            swerve::getPose, 
+            DriveConstants.kDriveKinematics, 
+            AutoConstants.kControllerX, 
+            AutoConstants.kControllerY, 
+            new PIDController(0, 0, 0), 
+            swerve::setModuleStates,
+            true,
+            swerve)
+        );
     }
 
-    public static FollowPathWithEvents getDefaultPathWithEvents(SwerveDrivetrain swerve){
+    public static FollowPathWithEvents getPathWithEvents(SwerveDrivetrain swerve){
     HashMap<String, Command> defaultEventMap = new HashMap<>();
     defaultEventMap.put("Event 1", new PrintCommand("Marker 1"));
     defaultEventMap.put("event 2", new PrintCommand("Marker 2"));
