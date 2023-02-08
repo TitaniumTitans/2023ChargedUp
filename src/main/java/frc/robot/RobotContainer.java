2959d86cfad62bf061d148a02f64fce0b3139a56
Hunter White <HWB94@protonmail.com>
2023-02-07 20:32:58 -0500

    Fixed subsystem naming.
    WristSubSystem.java would not build because the class name did not match the file name.
    
    WristSubSystem.java and ArmSubSystem.java have been renamed to use the name "Subsystem" instead of "SubSystem" like CameraSubsystem.java
    
    All code now builds.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,12 +11,11 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 // import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.robot.commands.MoveArmAngle;
 import frc.robot.commands.SwerveTeleopDrive;
 import frc.robot.commands.Autonomous.AutoUtils;
 import frc.robot.commands.Test.ArmToSetpoint;
 import frc.robot.subsystems.Arm.ArmIONeo;
-import frc.robot.subsystems.Arm.ArmSubSystem;
+import frc.robot.subsystems.Arm.ArmSubsystem;
 import frc.robot.subsystems.Swerve.SwerveDrivetrain;
 // import frc.robot.subsystems.Swerve.SwerveFalconIO;
 import frc.robot.subsystems.Swerve.SwerveNeoIO;
@@ -37,7 +36,7 @@ public class RobotContainer {
   //Subsystems
   private WristSubsystem m_wrist;
   private SwerveDrivetrain m_drive; 
-  private ArmSubSystem m_arm;
+  private ArmSubsystem m_arm;
 
   //Controllers
   private final CommandXboxController m_driveController = new CommandXboxController(Constants.DRIVER_PORT);
@@ -52,13 +51,13 @@ public class RobotContainer {
     case THANOS:
       m_drive = new SwerveDrivetrain(new SwerveNeoIO());
       m_wrist = new WristSubsystem(new WristIONeo());
-      m_arm = new ArmSubSystem(new ArmIONeo());
+      m_arm = new ArmSubsystem(new ArmIONeo());
       break;
     
     case HELIOS:
       m_drive = new SwerveDrivetrain(new SwerveNeoIO());
       m_wrist = new WristSubsystem(new WristIONeo());
-      m_arm = new ArmSubSystem(new ArmIONeo());
+      m_arm = new ArmSubsystem(new ArmIONeo());
       break;
 
     case SIM:
