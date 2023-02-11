1138662a41965e4eaea78155cabf13ba23fae2f9
Hunter White <HWB94@protonmail.com>
2023-02-10 19:20:33 -0500

    Updated AutoRetry

--- src/main/java/lib/utils/drivers/CTREUtil.java
+++ src/main/java/lib/utils/drivers/CTREUtil.java
@@ -2,7 +2,6 @@ package lib.utils.drivers;
 
 import com.ctre.phoenix.ErrorCode;
 import edu.wpi.first.wpilibj.DriverStation;
-
 public class CTREUtil {
     public interface ConfigCall {
         abstract ErrorCode run();
@@ -24,21 +23,35 @@ public class CTREUtil {
         }
     }
 
-    public static void autoRetry(ConfigCall configCall) {
-        int i;
-        ErrorCode err;
-        for (i = 0; hasError(err = configCall.run()) && i < MAX_RETRY_COUNT; i++) {
-            DriverStation.reportWarning(String.format("Try #%d failed: %s. Retrying...", i, err.toString()), false);
-            try {
-                Thread.sleep(RETRY_DELAY_MS);
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
+    public static ErrorCode autoRetry(ConfigCall configCall) {
+        ErrorCode err = ErrorCode.GeneralError; // Use an error be default, this will be overridden on the first pass
+
+        // Loop for MAX_RETRY_COUNT
+        for (int i = 0; i < MAX_RETRY_COUNT; i++) {
+            err = configCall.run();
+
+            // If there is an error, wait for a short period and try again.
+            // Otherwise, break the loop
+            if(hasError(err)) {
+                //DriverStation.reportWarning(String.format("Try #%d failed: %s. Retrying...", i+1, err.toString()), false);
+                try {
+                    // Some tools recommended against this, but because we are dealing with hardware, this should be sufficient.
+                    Thread.sleep(RETRY_DELAY_MS);
+                } catch (InterruptedException ignored) {
+                    // For some reason, the thread was intentionally interrupted
+                    // Follow through with the interrupt
+                    Thread.currentThread().interrupt();
+                }
+            } else {
+                break;
             }
         }
-        if (i > MAX_RETRY_COUNT) {
-            DriverStation.reportError("Error: Maximum retry count exceeded.", false);
-        } else if (i > 0) {
-            DriverStation.reportWarning(String.format("Try #%d succeeded!", i), false);
+
+        // If err is still an error, then the loop reached MAX_RETRY_COUNT and could not set the config
+        if (hasError(err)) {
+            // Display the error to the driver with stack trace to see WHAT failed to configure.
+            DriverStation.reportError("Failed to configure after " + MAX_RETRY_COUNT + "counts.",  true);
         }
+        return err;
     }
 }
