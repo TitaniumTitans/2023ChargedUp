c98548e140de8f6071e2d184f243e3ac784639fe
Steggoo <galacticraf5@gmail.com>
2023-02-17 21:00:23 -0500

    Feat: Added dashboard variables for arm extension

--- src/main/java/frc/robot/subsystems/arm/ArmExtSubsystem.java
+++ src/main/java/frc/robot/subsystems/arm/ArmExtSubsystem.java
@@ -7,7 +7,8 @@ import com.revrobotics.RelativeEncoder;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -17,6 +18,11 @@ public class ArmExtSubsystem extends SubsystemBase {
     private final RelativeEncoder m_relativeEncoderArmEx;
     private final PIDController m_extPID;
     private final DigitalInput m_armLimitSwitch;
+    // Logging variables
+    private double prevSetpointRaw;
+    private double prevSetpointClamped;
+    private double prevSetpointPID;
+    private ShuffleboardTab armExtTab;
 
     public ArmExtSubsystem() {
         m_ArmEx = new CANSparkMax(Constants.ArmConstants.ARM_EXTENSION_ID, CANSparkMaxLowLevel.MotorType.kBrushless);
@@ -28,6 +34,8 @@ public class ArmExtSubsystem extends SubsystemBase {
         m_extPID.setTolerance(0.5);
 
         m_armLimitSwitch = new DigitalInput(Constants.ArmConstants.LIMIT_SWITCH_PORT);
+
+        armExtTab = Shuffleboard.getTab("ArmExtSubsystem");
     }
 
     public void setArmSpeed(double speed) {
@@ -43,18 +51,18 @@ public class ArmExtSubsystem extends SubsystemBase {
         return runOnce(() -> {setArmSpeed(speed);});
     }
 
-    public void setArmExtension(double extension) {
-        double setpoint = MathUtil.clamp(extension, Constants.ArmConstants.EXT_LOWER_LIMIT, Constants.ArmConstants.EXT_HIGHER_LIMIT);
-        double pidOutput = MathUtil.clamp(m_extPID.calculate(getArmExtension(), setpoint), -0.5, 0.5);
+    public void setArmExtension(double targetExtRaw) {
+        double targetExtClamped = MathUtil.clamp(targetExtRaw, Constants.ArmConstants.EXT_LOWER_LIMIT, Constants.ArmConstants.EXT_HIGHER_LIMIT);
+        double targetExtPID = MathUtil.clamp(m_extPID.calculate(getArmExtension(), targetExtClamped), -0.5, 0.5);
 
-        SmartDashboard.putNumber("Extension Setpoint", setpoint);
-        SmartDashboard.putNumber("PID Output", pidOutput);
-        SmartDashboard.putBoolean("At Lower Limit", armAtLowerLimit());
+        prevSetpointRaw = targetExtRaw;
+        prevSetpointClamped = targetExtClamped;
+        prevSetpointPID = targetExtPID;
 
-        if(armAtLowerLimit() && pidOutput <= 0){
+        if(armAtLowerLimit() && targetExtPID <= 0){
             m_ArmEx.set(0);
         } else {
-            m_ArmEx.set(pidOutput);
+            m_ArmEx.set(targetExtPID);
         }
     }
 
@@ -66,12 +74,41 @@ public class ArmExtSubsystem extends SubsystemBase {
         return !m_armLimitSwitch.get();
     }
 
-    public void resetExstentionEncoder() {
+    public boolean armAtUpperLimit() {
+        return (m_relativeEncoderArmEx.getPosition() >= Constants.ArmConstants.EXT_HIGHER_LIMIT);
+    }
+
+    public boolean debugEncoderAtLowerLimit() {
+        return (m_relativeEncoderArmEx.getPosition() <= Constants.ArmConstants.EXT_LOWER_LIMIT);
+    }
+
+    public void resetExtensionEncoder() {
         m_relativeEncoderArmEx.setPosition(0.0);
     }
 
-    public boolean armExstensionAtSetpoint() {
+    public boolean armExtensionAtSetpoint() {
         return m_extPID.atSetpoint();
+    }
+
+    @Override
+    public void periodic() {
+        // Booleans
+        // Misc.
+        armExtTab.add("At setpoint", armExtensionAtSetpoint());
+        armExtTab.add("Motor inverted", m_ArmEx.getInverted());
+        // Limits
+        armExtTab.add("Limit switch triggered", armAtLowerLimit());
+        armExtTab.add("At upper limit", armAtUpperLimit());
+        armExtTab.add("At encoder lower than limit", debugEncoderAtLowerLimit());
+
+        // Doubles
+        // Angles
+        armExtTab.add("Encoder raw",m_relativeEncoderArmEx);
+        armExtTab.add("Extension converted", getArmExtension());
+        // Targets
+        armExtTab.add("Target", prevSetpointRaw);
+        armExtTab.add("Clamped setpoint", prevSetpointClamped);
+        armExtTab.add("PID setpoint output", prevSetpointPID);
     }
 }
 
