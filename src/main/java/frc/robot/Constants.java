a318e0cf3db4ee80b8d1b5bf5158c73f008d3a35
Steggoo <galacticraf5@gmail.com>
2023-02-22 19:51:17 -0500

    Fix: fixed camera offsets to be counterclockwise

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -71,7 +71,7 @@ public final class Constants {
         public static final Transform3d FRONT_CAM_POSE = new Transform3d
                 (new Translation3d(13.0, 4.0, 25.0), new Rotation3d(0.0, 5.0, 0.0));
         public static final Transform3d LEFT_CAM_POSE = new Transform3d
-                (new Translation3d(4.0, 6.0, 25.0), new Rotation3d(270.0, 0.0, 270.0));
+                (new Translation3d(4.0, 6.0, 25.0), new Rotation3d(90.0, 0.0, 90.0));
         public static final String FRONT_CAM_NAME = "FrontPiCam";
         public static final String LEFT_CAM_NAME = "LeftWebCam";
     }
