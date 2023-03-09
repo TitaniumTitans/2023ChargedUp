3399405074697db5488c456ddc711a489061c28d
GearBoxFox <98189655+GearBoxFox@users.noreply.github.com>
2023-03-08 19:18:45 -0500

    Feat: checking for zones based on alliance

--- src/main/kotlin/lib/utils/zone/Zone.kt
+++ src/main/kotlin/lib/utils/zone/Zone.kt
@@ -4,15 +4,15 @@ import edu.wpi.first.math.geometry.Pose2d
 import edu.wpi.first.math.geometry.Translation2d
 import lib.utils.piecewise.Range
 
-class Zone(topleft: Translation2d, width: Double, height: Double) {
+class Zone(topleft: Translation2d, bottomRight: Translation2d) {
 
     val leftRange: Range
     val topRange: Range
     init {
         //Construct a "rectangle" for zone based off the uppermost left point
         //Make sure all units are in meters
-        leftRange = Range(topleft.x, true, topleft.y - height, true)
-        topRange = Range(topleft.y, true, topleft.x - width, true)
+        leftRange = Range(topleft.y, true, bottomRight.y, true)
+        topRange = Range(topleft.x, true, bottomRight.x, true)
     }
 
     fun inZone(pose: Pose2d): Boolean {
