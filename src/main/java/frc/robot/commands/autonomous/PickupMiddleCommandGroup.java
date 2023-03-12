eb150b2cacc9531c370d7fe9eb6608f05dddcf47
Hunter White <HWB94@protonmail.com>
2023-03-12 16:31:22 -0400

    Pickup Middle auto can now score middle or high. Adjusted path.

--- src/main/java/frc/robot/commands/autonomous/PickupMiddleCommandGroup.java
+++ src/main/java/frc/robot/commands/autonomous/PickupMiddleCommandGroup.java
@@ -8,23 +8,34 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants;
 import frc.robot.commands.SupersystemToPoseAutoCommand;
 import frc.robot.subsystems.swerve.SwerveDrivetrain;
+import frc.robot.supersystems.ArmPose;
 import frc.robot.supersystems.ArmSupersystem;
 
 import java.util.HashMap;
+import java.util.Objects;
 
 public class PickupMiddleCommandGroup extends SequentialCommandGroup {
-    public PickupMiddleCommandGroup(SwerveDrivetrain m_swerve, ArmSupersystem m_armSupersystem) {
+    public PickupMiddleCommandGroup(SwerveDrivetrain m_swerve, ArmSupersystem m_armSupersystem, AutoUtils.ScoringHeights scoreHeight) {
         addRequirements(m_swerve);
         m_armSupersystem.addRequirements(this);
 
         PathConstraints constraints = AutoUtils.getDefaultConstraints();
         PathPlannerTrajectory trajectory = PathPlanner.loadPath("PickUp Middle", constraints);
 
+        ArmPose armScoringPose;
+
+        // We don't score low (for now at least)
+        if (scoreHeight == AutoUtils.ScoringHeights.MIDDLE) {
+            armScoringPose = Constants.ArmSetpoints.MIDDLE_GOAL_NON_STOW;
+        } else {
+            armScoringPose = Constants.ArmSetpoints.HIGH_GOAL;
+        }
+
         HashMap<String, Command> autoEvents = new HashMap<>();
         autoEvents.put("LowerIntake", new SupersystemToPoseAutoCommand(m_armSupersystem, Constants.ArmSetpoints.INTAKE_CUBE));
         autoEvents.put("Pickup", m_armSupersystem.runIntakeForTime(0.3));
         autoEvents.put("ClearGround", new SupersystemToPoseAutoCommand(m_armSupersystem, Constants.ArmSetpoints.STOW_POSITION));
-        autoEvents.put("Score", new SupersystemToPoseAutoCommand(m_armSupersystem, Constants.ArmSetpoints.HIGH_GOAL));
+        autoEvents.put("Score", new SupersystemToPoseAutoCommand(m_armSupersystem, armScoringPose));
 
         addCommands(m_swerve.getAutoBuilder(autoEvents).fullAuto(trajectory));
     }
